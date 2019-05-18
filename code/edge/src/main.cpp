313d452859b130a726fbd21df0fd4c18ee49c67b
Daniel Robinson <d7robin@gmail.com>
2019-05-18 03:16:17 +0200

    serTIM, guess_seq_len

--- code/edge/src/main.cpp
+++ code/edge/src/main.cpp
@@ -2,10 +2,12 @@
 
 #define minIdleTime 1000
 
-volatile uint32_t lastEdgeTime, firstEdgeTime, idleTime, txTime;
+volatile uint32_t lastEdgeTime, firstEdgeTime, idleTime, txTime, counter;
 volatile boolean triggered, printOutput;
 
 String logMessage = "";
+
+void servicePulses();
 
 void setup()
 {
@@ -25,6 +27,17 @@ void setup()
 
 void loop()
 {
+    servicePulses();
+
+    if (Serial.available()) {
+        char c = Serial.read();
+        if (c == 'r') {
+            counter = 0;
+        }
+    }
+}
+
+void servicePulses() {
     uint32_t ms = millis();   
     boolean afterIdle = (ms - lastEdgeTime) > minIdleTime;
     if (triggered) {
@@ -37,7 +50,7 @@ void loop()
         lastEdgeTime = ms;
     } else if (afterIdle && printOutput) {
         txTime = lastEdgeTime - firstEdgeTime;
-        logMessage = String(idleTime + txTime) + ", " + String(idleTime) + ", " + String(txTime);
+        logMessage = String(idleTime + txTime) + ", " + String(idleTime) + ", " + String(txTime) + ", " + ++counter;
         Serial.println(logMessage);
         printOutput = false;
         triggered = false;
@@ -49,71 +62,3 @@ ISR(ANALOG_COMP_vect)
     digitalWrite(13, !digitalRead(13));
     triggered = true;
 }
-
-// uint32_t seconds = 0;
-// uint8_t threshVolt = 0.3 * 255 / 5;
-// uint32_t tRise = 0, tFall = 0;
-
-// int currentEdge = 0, lastEdge = 0;
-
-// boolean pulsed = false;
-// volatile boolean triggered = false;
-
-// ISR (ANALOG_COMP_vect)
-// {
-// 	triggered = true;
-// }
-
-// boolean onSecond() {
-//     uint32_t s = millis()/1000;
-
-//     if (s != seconds) {
-//         seconds = s;
-//         return true;
-//     }
-//     return false;
-// }
-
-// void setup () {
-//     Serial.begin(115200);
-//     pinMode(A0, INPUT);
-
-//     ADCSRB = 0;        // (Disable) ACME: Analog Comparator Multiplexer Enable
-//     ACSR = bit(ACI)    // (Clear) Analog Comparator Interrupt Flag
-//            | bit(ACIE) // Analog Comparator Interrupt Enable
-//            | bit(ACBG)
-//            | bit(ACIS0); // ACIS1, ACIS0: Analog Comparator Interrupt Mode Select (trigger on falling edge)
-// }
-
-// void loop() {
-
-//     int edge = analogRead(A0) > threshVolt;
-
-//     if (triggered) {
-//         Serial.println("triggered");
-//         triggered = false;
-//     }
-
-//     if (edge != lastEdge) {
-//         if (!edge) {
-//             tFall = millis();
-//         }
-
-//         if ((millis() - tFall) > 50) {
-//             // if (!pulsed) {
-//             //     Serial.print(tFall - tRise); Serial.print(", ");
-//             //     pulsed = true;
-//             // }
-//             if (edge && (millis() - tRise > 1000)) {
-//                 // pulsed = false;
-//                 tRise = millis();
-//                 int diff = tRise - tFall;
-//                 Serial.print(2540 - diff % 2540);
-//                 Serial.print(", ");
-//                 Serial.println(diff);
-//             }
-//         }
-//     }
-
-//     lastEdge = edge;
-// }
--- code/test_eDRX.py
+++ code/test_eDRX.py
@@ -1,14 +1,16 @@
-import unittest
 import serial
 import colorama
 import sys
 import time
 import pytest
+import numpy as np
+import math
 
 from process import timing_bits as p
 
 colorama.init()
-ser = serial.Serial('COM108', 9600, timeout=1)
+serAT = serial.Serial('COM108', 9600, timeout=1)
+serTIM = serial.Serial('COM15', 115200, timeout=1)
 to_bin = lambda x, n: format(x, 'b').zfill(n)
 
 black   =  '\033[1;30m'
@@ -24,17 +26,43 @@ nw_edrx = ["0010","0011","0010","0101","0010","1001","1010","1011","1100","1101"
 nw_ptw  = ["0000","0000","0000","0000","0000","0000","0000","0000","0000","0000","0000","0000","0001","0001","0001","0001","0001","0001","0001","0001","0001","0001","0001","0001","0010","0010","0010","0010","0010","0010","0010","0010","0010","0010","0010","0010","0011","0011","0011","0011","0011","0011","0011","0011","0011","0011","0011","0011","0100","0100","0100","0100","0100","0100","0100","0100","0100","0100","0100","0100","0101","0101","0101","0101","0101","0101","0101","0101","0101","0101","0101","0101","0110","0110","0110","0110","0110","0110","0110","0110","0110","0110","0110","0110","0111","0111","0111","0111","0111","0111","0111","0111","0111","0111","0111","0111","1000","1000","1000","1000","1000","1000","1000","1000","1000","1000","1000","1000","1001","1001","1001","1001","1001","1001","1001","1001","1001","1001","1001","1001"]
 
 print (yellow, 'Welcome to the', green, 'AT command', yellow, 'tester by Daniel Robinson.')
-    
-def send(cmd, t=0, expect='OK'):
-    print(yellow + cmd)
-    ser.write(bytes(cmd + '\r', 'utf-8'))
-    return receive(t, expect)
 
-def receive(t=0, expect='OK'):
+def sendTIM(cmd):
+    print(yellow + cmd)
+    serTIM.write(bytes(cmd + '\r', 'utf-8'))
+
+def receiveTIM():
+    c = 0
+    a = []
+    b = []
+    g = 0
+    while True:
+        d = serTIM.readline().decode('utf-8')
+        if not len(d):
+            c += 1
+        else:
+            d = d.strip()
+            a.append(d)
+            b.append(int(d.split(',')[0]))
+            print(white + d, blue + str(b))
+            g = guess_seq_len(b)
+            if g != 0:
+                print(magenta + str(g))
+                break
+
+def test_rec():
+    receiveTIM()
+    
+def sendAT(cmd, t=0, expect='OK'):
+    print(yellow + cmd)
+    serAT.write(bytes(cmd + '\r', 'utf-8'))
+    return receiveAT(t, expect)
+
+def receiveAT(t=0, expect='OK'):
     c = 0
     data = []
     while True:
-        d = ser.readline().decode('utf-8')
+        d = serAT.readline().decode('utf-8')
         if not len(d):
             c += 1
         d = d.strip()
@@ -52,16 +80,16 @@ def receive(t=0, expect='OK'):
             return data
 
 def OK(cmd, t=0):
-    assert 'OK' in send(cmd, t)
+    assert 'OK' in sendAT(cmd, t)
 
 def expect(cmd, reply, t=1):
-    data = send(cmd, t, reply)
+    data = sendAT(cmd, t, reply)
     assert True in [reply in i for i in data]
     return data
     
 @pytest.mark.setup
 def test_connect():
-    assert ser.is_open == True
+    assert serAT.is_open == True
 
 @pytest.mark.setup
 def test_AT():
@@ -85,7 +113,7 @@ def test_CFUN():
     OK('AT+CFUN=0', 3)
     expect('at+cfun?', '+CFUN: 0', 1)
     expect('AT+CFUN=1', '+CEREG: 0', 2)
-    receive()
+    receiveAT()
 
 @pytest.mark.setup
 def test_COPS():
@@ -95,11 +123,11 @@ def test_COPS():
 def test_CEREG():
     expect('AT+CEREG?', '+CEREG: 5,1')
 
-@pytest.mark.ping
+@pytest.mark.setup
 def test_ping():
     expect('at+nping="8.8.8.8"', '+NPING: "8.8.8.8"', 10)
 
-@pytest.mark.release
+@pytest.mark.setup
 def test_release():
     expect('at+cops=2', '+NPSMR: 1', 10)
     expect('at+cops=0', '+CEREG: 1', 10)
@@ -112,14 +140,14 @@ def test_eDRX():
     for edrx in range(10):
         for ptw in range(16):
             data = expect('AT+NPTWEDRXS=2,5,"' + str(to_bin(ptw, 4)) + '","' + str(to_bin(edrx, 4)) + '"', '+CSCON: 0', 10)
-            receive(2, expect='+NPTWEDRXP')
+            receiveAT(2, expect='+NPTWEDRXP')
             expect('AT+NPTWEDRXS?', 'NPTWEDRXS')
 
 @pytest.mark.edrx_nw
 def test_NWeDRX():
     for ptw, edrx in zip(nw_ptw, nw_edrx):
         data = expect('AT+NPTWEDRXS=2,5,"' + ptw + '","' + edrx + '"', '+CSCON: 0', 10)
-        # receive(2, expect='+NPTWEDRXP')
+        # receiveAT(2, expect='+NPTWEDRXP')
         # expect('AT+NPTWEDRXS?', 'NPTWEDRXS')
 
 
@@ -133,9 +161,33 @@ def test_file_eDRX():
         print(p.csv(l))
         fx.write(p.csv(l) + '\n')
 
+def guess_seq_len(seq):
+    # find number of sequences in similar arrays
+    # limited to a maximum difference
+    maxDiff = 100
+    guess = 0
+    max_len = math.ceil(len(seq) / 2)
+    for s in range(len(seq)):
+        for x in range(1, max_len - s + 1):
+            a = abs(np.array(seq[s:s+x]) - np.array(seq[s+x:s+2*x]))
+            if len(a) and not False in (a < np.array([maxDiff] * len(a))):
+                print(x, s, a, seq[s:s+x], "==", seq[s+x:s+2*x])
+                return x
+    return guess
+
+def test_findRepetition():
+    f = open("logs/pulses0x1331NWeDRX81.92PTW5.21.log", "r")
+    lines = f.readlines()
+    a = []
+    for l in lines:
+        a.append(int(l.split(',')[0]))
+    print(guess_seq_len(a))
+
 def test_close():
-    ser.close()
-    assert ser.is_open == False
+    serAT.close()
+    serTIM.close()
+    assert serAT.is_open == False
+    assert serTIM.is_open == False
 
 if __name__ == '__main__':
     test_close()
